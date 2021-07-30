ccf32f483ebdf4df89dda9a838c9de9221816d11
HuzefaC <huzefachabukswar@gmail.com>
2021-07-30 11:51:22 +0530

    Add Thumb and HeroImage component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,9 @@ import React, { useState, useEffect } from 'react';
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../../config';
 
 // Components
+import HeroImage from '../../components/HeroImage/HeroImage.component';
+import Grid from '../../components/Grid/Grid.component';
+import Thumb from '../../components/Thumb/Thumb.component';
 
 // Hook
 import { useHomeFetch } from '../../hooks/useHomeFetch';
@@ -16,7 +19,31 @@ const Home = () => {
 
   console.log(state);
 
-  return <div>Home Page</div>;
+  return (
+    <>
+      {state.results[0] ? (
+        <HeroImage
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+          title={state.results[0].original_title}
+          text={state.results[0].overview}
+        />
+      ) : null}
+      <Grid header="Popular Movies">
+        {state.results.map((movie) => (
+          <Thumb
+            key={movie.id}
+            clickable={true}
+            image={
+              movie.poster_path
+                ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                : NoImage
+            }
+            movieId={movie.id}
+          />
+        ))}
+      </Grid>
+    </>
+  );
 };
 
 export default Home;
