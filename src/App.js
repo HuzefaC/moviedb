5ed9191649371f265ec9a7a1f0de1a0a48a13112
HuzefaC <huzefachabukswar@gmail.com>
2021-07-30 10:31:33 +0530

    Add Header component and Home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
+// Components
+import Header from './components/Header/Header.component';
+import Home from './pages/Home/Home';
+
+// Styles
+import { GlobalStyle } from './GlobalStyle';
+
 function App() {
   return (
     <div className="App">
-      Start here.
+      <Header />
+      <Home />
+      <GlobalStyle />
     </div>
   );
 }
